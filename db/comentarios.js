93930adae675176ce3e2fe8aef8135cf07f93fbc
cristiano Banfi <cbanfi@udesa.edu.ar>
2022-04-06 14:25:27 -0300

    cambios cristiano

--- db/comentarios.js
+++ db/comentarios.js
@@ -2,23 +2,23 @@ let comentarios = {
     
     lista: [
         {
-            nombreDelUsuario: '',
-            textoDelComentario: "",
+            nombreDelUsuario: 'Martin',
+            textoDelComentario: "Hermosa guitarra, me encontré con lo que esperaba, gran volumen de caja (me sorprendió eso).",
             imagenDelPerfil: '',
         },
         {
-            nombreDelUsuario: '',
-            textoDelComentario: '',
+            nombreDelUsuario: 'Clara',
+            textoDelComentario: 'Muy lindo producto, linda terminación. Recien empiezo a probarla. En sonido que da me encanta y el color muy lindo.',
             imagenDelPerfil: '',
         },
         {
-            nombreDelUsuario: '',
-            textoDelComentario: '',
+            nombreDelUsuario: 'Tomas',
+            textoDelComentario: 'El teclado buenísimo, tenía miedo de su calidad ya que al ser midi, con tantas octavas y tan barato a comparación de otros teclados me generaba desconfianza. Cuando lo probé me sorprendí del sonido muy nítido, bastantes tonos y con buen efecto, también el volumen es genial! lo único malo es que hay que comprar el enchufe por separado pero el resto 10/10 en calidad',
             imagenDelPerfil: '',
         },
         {
-            nombreDelUsuario: '',
-            textoDelComentario: '',
+            nombreDelUsuario: 'Marcos',
+            textoDelComentario: 'Creo que sirve para empezar el estudio musical de un niño, tiene bastantes funciones y es buena calidad.',
             imagenDelPerfil: '',
         },
     
