a880ea9cd1d5afdf8036211fbfa42580d01d1cae
GianfrancoGrillo <89219968+GianfrancoGrillo@users.noreply.github.com>
2022-04-05 22:51:41 -0300

    rutas de productos y busqueda

--- proyectoParcial/app.js
+++ proyectoParcial/app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+let productosRouter = require('./routes/product');
+let productoAddRouter = require('./routes/product-add')
+let searchRouter = require('./routes/search-results');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use('/productos',productosRouter,productoAddRouter);
+app.use('/search-results',searchRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ proyectoParcial/routes/product-add.js
@@ -0,0 +1,7 @@
+let express = require('express');
+let router = express.Router();
+
+
+router.get('/add',) ;
+
+module.exports = router;
