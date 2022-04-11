ff7f31f1ebde4348893d47e21ad1f2ef7053b7f0
jacintaVillanueva <jvillanueva@udesa.edu.ar>
2022-04-11 16:42:23 -0300

    arreglo header

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 let productosRouter = require('./routes/product');
 let searchRouter = require('./routes/search-results');
+const usersController = require('./Controladores/usersControllers');
 
 var app = express();
 
