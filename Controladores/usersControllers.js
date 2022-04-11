fe21a0096e8090ff352a0b503ba4a7a859d1f450
cristiano Banfi <cbanfi@udesa.edu.ar>
2022-04-11 09:30:50 -0300

    creo controlador y rutas de user

---
+++ Controladores/usersControllers.js
@@ -0,0 +1,19 @@
+let usuariosU = require('../db/usuarios');
+
+
+const usersController = {
+
+profile: function (req, res) {
+    let idProducto = req.params.id
+return res.render ('profile',{profile: usuariosU.lista})
+}, 
+
+profileEdit: function (req, res) {
+return res.render ('profile-edit',{profileEdit: usuariosU.lista})
+
+},
+
+
+}
+
+module.exports = usersController;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+let express = require('express');
+let router = express.Router();
+let usersControllerr = require('../Controladores/usersControllers')
+
+router.get('/profile/:id',usersControllerr.profile);
+
+router.get('/add',usersControllerr.profileEdit); 
 
 module.exports = router;
