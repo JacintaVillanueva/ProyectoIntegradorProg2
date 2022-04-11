9b36ba0f2d7cca325900ac68c6598e9b7835da63
GianfrancoGrillo <89219968+GianfrancoGrillo@users.noreply.github.com>
2022-04-11 11:22:29 -0300

    index

--- Controladores/controladorProducts.js
+++ Controladores/controladorProducts.js
@@ -5,7 +5,7 @@ const productController = {
 
 index: function (req, res) {
     let idProducto = req.params.id
-return res.render ('product',{product: productosP.lista})
+return res.render ('product',{product: idProducto})
 }, 
 
 create: function (req, res) {
