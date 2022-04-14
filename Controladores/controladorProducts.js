221d6287bf98481e710bec8f59d821159399f311
GianfrancoGrillo <89219968+GianfrancoGrillo@users.noreply.github.com>
2022-04-13 21:01:02 -0300

    cambios en comentarios

--- Controladores/controladorProducts.js
+++ Controladores/controladorProducts.js
@@ -1,11 +1,11 @@
 let productosP = require('../db/productos');
-
+let comentariosC = require('../db/comentarios');
 
 const productController = {
 
 index: function (req, res) {
     let idProducto = req.params.id
-return res.render ('product',{product: idProducto})
+return res.render ('product',{product: idProducto, comments: comentariosC.lista})
 }, 
 
 create: function (req, res) {
@@ -13,7 +13,10 @@ return res.render ('productAdd',{product: productosP.lista})
 
 },
 
-
+/* comentarios: function (req, res) {
+    return res.render ('product',{comments: comentariosC.lista})
+}
+ */
 }
 
 module.exports = productController;
--- db/comentarios.js
+++ db/comentarios.js
@@ -4,22 +4,22 @@ let comentarios = {
         {
             nombreDelUsuario: 'Martin',
             textoDelComentario: "Hermosa guitarra, me encontré con lo que esperaba, gran volumen de caja (me sorprendió eso).",
-            imagenDelPerfil: '',
+            imagenDelPerfil: 'https://upload.wikimedia.org/wikipedia/commons/b/bb/Mart%C3%ADn_Guzm%C3%A1n_%28cropped%2C_portrait%29.jpg',
         },
         {
             nombreDelUsuario: 'Clara',
             textoDelComentario: 'Muy lindo producto, linda terminación. Recien empiezo a probarla. En sonido que da me encanta y el color muy lindo.',
-            imagenDelPerfil: '',
+            imagenDelPerfil: 'https://assets.iprofesional.com/cdn-cgi/image/w=880,f=webp/https://assets.iprofesional.com/assets/png/2020/01/490165.png',
         },
         {
             nombreDelUsuario: 'Tomas',
             textoDelComentario: 'El teclado buenísimo, tenía miedo de su calidad ya que al ser midi, con tantas octavas y tan barato a comparación de otros teclados me generaba desconfianza. Cuando lo probé me sorprendí del sonido muy nítido, bastantes tonos y con buen efecto, también el volumen es genial! lo único malo es que hay que comprar el enchufe por separado pero el resto 10/10 en calidad',
-            imagenDelPerfil: '',
+            imagenDelPerfil: 'https://upload.wikimedia.org/wikipedia/commons/c/c1/Lionel_Messi_20180626.jpg',
         },
         {
             nombreDelUsuario: 'Marcos',
             textoDelComentario: 'Creo que sirve para empezar el estudio musical de un niño, tiene bastantes funciones y es buena calidad.',
-            imagenDelPerfil: '',
+            imagenDelPerfil: 'https://i.pinimg.com/474x/00/2b/56/002b568d8c0d88d710080ed734ab5b1e.jpg',
         },
     
     ]
