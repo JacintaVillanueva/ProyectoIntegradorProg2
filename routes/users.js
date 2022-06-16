dd3f7f2fed83ebfb8c2f38fe882cc5ed6afc5a33
GianfrancoGrillo <89219968+GianfrancoGrillo@users.noreply.github.com>
2022-06-16 20:08:10 -0300

    multer y registro

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,31 @@
 
 let express = require('express');
 let router = express.Router();
+const multer = require('multer')
+const path = require('path');
 let usersControllerr = require('../Controladores/usersControllers')
 let registerControllerr = require('../Controladores/registroController')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+    cb(null, path.join(__dirname,"../public/images/avatars") )
+
+    },
+    filename: function (req, file, cb) {
+        cb(null,file.fieldname + "-" + Date.now()+ path.extname(file.original))
+    }
+})
+
+let upload = multer({storage: storage})
+
+
 
 router.get('/profile/:id',usersControllerr.profile);
 
 router.get('/add',usersControllerr.profileEdit); 
 router.get('/login',registerControllerr.login);
 router.get('/register',registerControllerr.register);
+router.post('/register',upload.single('avatar'),registerControllerr.store);
 router.post('/register',registerControllerr.store);
 router.get('/user/edit/:userId',);
 router.post('/user/edit',);
