e96d4a24014b3976d658f2d812694f1a9f409ed3
GianfrancoGrillo <89219968+GianfrancoGrillo@users.noreply.github.com>
2022-06-16 20:24:51 -0300

    multer

--- Controladores/registroController.js
+++ Controladores/registroController.js
@@ -58,7 +58,7 @@ let registerController = {
                         password: bcrypt.hashSync(req.body.password, 10),
                         fecha_nacimiento: req.body.fecha, 
                         numero_documento: req.body.dni,
-                        image_users: "n",              //arreglar porque no se sube la foto
+                        image_users: "nnn",              //arreglar porque no se sube la foto
                     }
                     console.log(user)
                     console.log(users)
